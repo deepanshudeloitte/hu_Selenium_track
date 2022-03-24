5d6f5d4cc8f8fafc819110054d2d00ab12f1fc75
Deepanshu <deepanshusingh8@deloitte.com>
2022-03-24 13:10:25 +0530

    Scenario1 and scenario2 uploaded successfully

---
+++ MiniAssignment 3/src/Scenarios/Scenario1.java
@@ -0,0 +1,24 @@
+package Scenarios;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+public class Scenario1 {
+    public static void main(String[] args) throws InterruptedException{
+        System.setProperty("webdriver.chrome.driver","C:\\Users\\deepanshusingh8\\Downloads\\chromedriver.exe");
+        WebDriver driver=new ChromeDriver();
+        driver.manage().window().maximize();
+
+        driver.get("https://the-internet.herokuapp.com/");
+        driver.findElement(By.xpath("//*[@id=\"content\"]/ul/li[22]/a")).click();
+        Thread.sleep(2000);
+        driver.findElement(By.xpath("//*[@id=\"content\"]/div/ul/li[1]/a")).click();
+        Thread.sleep(1000);
+        driver.switchTo().frame("frame-top");
+        driver.switchTo().frame("frame-left");
+        String title=driver.findElement(By.xpath("/html/body")).getText();
+        System.out.println(title);
+
+
+        driver.quit();
+    }
+}
---
+++ MiniAssignment 3/src/Scenarios/scenario2.java
@@ -0,0 +1,28 @@
+package Scenarios;
+import org.openqa.selenium.By;
+import org.openqa.selenium.ScreenOrientation;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.interactions.Actions;
+
+public class scenario2 {
+    public static void main(String[] args) {
+
+
+        System.setProperty("webdriver.chrome.driver", "C:\\Users\\deepanshusingh8\\Downloads\\chromedriver.exe");
+        WebDriver driver = new ChromeDriver();
+        driver.manage().window().maximize();
+
+        driver.get("https://jqueryui.com/droppable/");
+        Actions builder = new Actions(driver);
+        driver.switchTo().frame(driver.findElement(By.xpath("//iframe[@class='demo-frame']")));
+        WebElement source = driver.findElement(By.xpath("//div[@id='draggable']"));
+        WebElement dest = driver.findElement(By.xpath("//div[@id='droppable']"));
+
+        builder.dragAndDrop(source, dest).build().perform();
+
+        driver.switchTo().defaultContent();
+
+    }
+}
